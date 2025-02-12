7f88aa3b6611efbbbd331415d73bac82e275d8ef
Abdul Moiz <amoez897@gmail.com>
2025-02-12 20:45:43 +0500

    after solving bananas i am finally pushing code from ...

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
+// Get the directory name from the current module URL
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.PNG'],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'), // Correct usage of __dirname
     },
   },
+ 
   build: {
+    outDir: 'build', // Change this to 'build' if you want the output here
     assetsDir: 'assets',
     rollupOptions: {
       output: {
