3b860aab0d62740d9436d61e0be68988526e4e3e
Abdul Moiz <amoez897@gmail.com>
2025-02-16 23:46:17 +0500

    version1.3

--- vite.config.js
+++ vite.config.js
@@ -10,17 +10,37 @@ export default defineConfig({
   assetsInclude: ['**/*.PNG'],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Correct usage of __dirname
+      '@': path.resolve(__dirname, './src'),
     },
   },
- 
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName: '[name]__[local]___[hash:base64:5]'
+    }
+  },
   build: {
-    outDir: 'build', // Change this to 'build' if you want the output here
+    outDir: 'build',
     assetsDir: 'assets',
+    cssCodeSplit: true,
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name][extname]'
-      }
-    }
-  }
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          } else if (/css/i.test(extType)) {
+            extType = 'css';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+    sourcemap: true,
+    minify: 'terser'
+  },
+  base: '/'
 })
