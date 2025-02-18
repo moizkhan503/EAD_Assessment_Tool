ca4ac4b364cd5a51f89e3fe49d3f3766237a44c6
Abdul Moiz <amoez897@gmail.com>
2025-02-18 16:53:58 +0500

    testing version

--- vite.config.js
+++ vite.config.js
@@ -6,39 +6,25 @@ import path from 'path'
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 export default defineConfig({
+  base: 'https://dev.earlyagedevelopment.com/',
   plugins: [react()],
   assetsInclude: ['**/*.PNG'],
-<<<<<<< HEAD
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // Correct usage of __dirname
     },
   },
- 
   build: {
     outDir: 'build', // Change this to 'build' if you want the output here
-=======
-  base: 'https://dev.earlyagedevelopment.com/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'build',
->>>>>>> origin/main
     assetsDir: 'assets',
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name][extname]'
       }
     }
-<<<<<<< HEAD
-=======
   },
   server: {
     port: 3000,
     historyApiFallback: true
->>>>>>> origin/main
   }
-})
+});
